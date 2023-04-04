fb84465bf8d4e672db1c504fa9408bbc9c0e792d
Tudor Linca <linca.tudor@gmail.com>
2023-04-04 18:11:51 +0300

    Initial commit

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -1,13 +1,13 @@
-import * as admin from "firebase-admin";
-import * as functions from "firebase-functions";
+import * as admin from 'firebase-admin';
+import * as functions from 'firebase-functions';
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    privateKey: functions.config().private.key.replace(/\\n/g, "\n"),
+    privateKey: functions.config().private.key.replace(/\\n/g, '\n'),
     projectId: functions.config().project.id,
     clientEmail: functions.config().client.email,
   }),
-  databaseURL: "https://registryapp-backend-84c2e.firebaseio.com",
+  databaseURL: process.env.DATABASE_URL,
 });
 
 const db = admin.firestore();
